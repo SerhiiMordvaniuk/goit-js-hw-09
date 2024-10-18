457fa1c9a3907bcc14e245ad9363d32120ba171d
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-18 16:41:41 +0200

    aaa

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,27 @@
- 
+
+const formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector(".feedback-form");
+
+const formMessage = document.querySelector(".txtarea");
+const formEmail = document.querySelector(".email")
+
+
+form.addEventListener("input", (event) => {
+    formData.email = event.currentTarget.email.value;
+    formData.message = event.currentTarget.message.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+})
+
+form.addEventListener("submit", (event) => {
+    if (event.currentTarget.email.value == "" || event.currentTarget.message.value == "") {
+        alert("Fill please all fields")
+    }
+    event.preventDefault();
+    localStorage.removeItem("feedback-form-state")
+    console.log(formData);
+    form.reset()
+})
