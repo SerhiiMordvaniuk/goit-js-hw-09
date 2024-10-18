7fe9035ba9a6eec9bbc10971d01d424bc229a381
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-18 17:28:40 +0200

    aaa

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,9 +5,20 @@ const formData = {
 };
 
 const form = document.querySelector(".feedback-form");
-
 const formMessage = document.querySelector(".txtarea");
 const formEmail = document.querySelector(".email")
+
+
+const localData = JSON.parse(localStorage.getItem("feedback-form-state"))
+function checkLocalData() {
+    if (typeof localData !== "object") {
+        return
+    }
+    formMessage.value = localData.message
+        formEmail.value = localData.email
+}
+
+
 
 
 form.addEventListener("input", (event) => {
@@ -18,10 +29,15 @@ form.addEventListener("input", (event) => {
 
 form.addEventListener("submit", (event) => {
     if (event.currentTarget.email.value == "" || event.currentTarget.message.value == "") {
-        alert("Fill please all fields")
+        alert("Fill please all fields")        
     }
     event.preventDefault();
     localStorage.removeItem("feedback-form-state")
-    console.log(formData);
+    if (typeof event.currentTarget.email.value == "string" && typeof event.currentTarget.message.value == "string") {
+        console.log(formData);
+    }
     form.reset()
+    console.log(typeof event.currentTarget.message.value);
 })
+
+
